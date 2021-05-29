eb6008b075cc9350658c733dca2ba29ff9679956
CristianHourcade [goiar] <hourcadecristian@gmail.com>
2021-05-29 17:24:36 -0300

    Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomeContainer from './containers/HomeContainer';
 import ProductsContainer from './containers/ProductsContainer';
 import { EcommerceProvider } from './context/EcommerceContext';
+import { addElementToCart } from './redux/actions/cart';
 
 const App = () => {
+  const STATE = useSelector(state => state.cartReducer);
+  const dispatch = useDispatch();
+
+  console.log(STATE);
+  
   return (
     <div className="App">
+      <button onClick={() => { dispatch(addElementToCart({ id: 1, name: 'shirt', price: 3000 })) }}>
+        Agregar al carrito
+        </button>
       <BrowserRouter>
         <EcommerceProvider>
           <Switch>
